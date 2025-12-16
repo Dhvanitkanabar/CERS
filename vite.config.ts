2be1e60db09516c88943baf5c5bdcf91737d5f22
Dhvanit1212 <dhvanitkanabar@gmail.com>
2025-12-16 10:25:04 +0530

    error resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // if using React
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 3000
+    host: true, // allows external access
+  },
+  preview: {
+    allowedHosts: ['cers-4.onrender.com']
   }
 })
